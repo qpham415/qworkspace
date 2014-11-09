cc9285571af054e9de05586d5c47f27b8bea8d38
q <qpham415@gmail.com>
2014-11-09 00:24:29 -0800

    gallery styling, plus add page bottom

--- wp-content/themes/hipaccounting/gallery-page.php
+++ wp-content/themes/hipaccounting/gallery-page.php
@@ -4,41 +4,40 @@ Template Name: Gallery
 */
 ?>
 <?php get_header(); ?>
-    <div id="content">
-      <?php the_post(); ?>
+  <div id="content">
+    <?php the_post(); ?>
 
-      <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-       	<section id="gallery">
-       		<div class="container">
-            	<div class="row">
-                	<div class="col-lg-12 text-center">
-                    	<h2 class="uppercase">Our Works Gallery</h2>
-                        <h3>Suspendisse vel tempor neque sed feugiat felis at facilisis commodo.</h3>
-                    </div>
+    <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+      <section id="gallery">
+       	<div class="container">
+          <div class="row">
+            <div class="col-lg-12 text-center">
+              <?php the_content(); ?>
+            </div>
 
-                    <div class="gallery col-lg-12">
+            <div class="gallery col-lg-12">
 
-						<div class="item">
-							<figure class="effect-bubba">
-								<img class="img-responsive" src="<?php echo $key_1_value = get_post_meta( get_the_ID(), 'gallerypic01', true );?>" alt="">
-								<figcaption>
-									<h2>Sed <span>nulla</span></h2>
-									<p>Sed at convallis erat</p>
-									<a class="image-link" href="<?php echo $key_1_value = get_post_meta( get_the_ID(), 'gallerypic01', true );?>">View more</a>
-								</figcaption>
-							</figure>
-                        </div>
+						  <div class="item">
+							  <figure class="effect-bubba">
+								  <img class="img-responsive" src="<?php echo $key_1_value = get_post_meta( get_the_ID(), 'gallerypic01', true );?>" alt="">
+								  <figcaption>
+									  <h2>Sed <span>nulla</span></h2>
+									  <p>Sed at convallis erat</p>
+									  <a class="image-link" href="<?php echo $key_1_value = get_post_meta( get_the_ID(), 'gallerypic01', true );?>">View more</a>
+								  </figcaption>
+							  </figure>
+              </div>
 
-                        <div class="item">
-                            <figure class="effect-bubba">
-								<img class="img-responsive" src="<?php echo $key_1_value = get_post_meta( get_the_ID(), 'gallerypic02', true );?>" alt="">
-								<figcaption>
-									<h2>dolor <span>nibh</span></h2>
-									<p>Sed at convallis erat</p>
-									<a class="image-link" href="<?php echo $key_1_value = get_post_meta( get_the_ID(), 'gallerypic02', true );?>">View more</a>
-								</figcaption>
-                            </figure>
-                        </div>
+              <div class="item">
+                <figure class="effect-bubba">
+								  <img class="img-responsive" src="<?php echo $key_1_value = get_post_meta( get_the_ID(), 'gallerypic02', true );?>" alt="">
+								  <figcaption>
+									  <h2>dolor <span>nibh</span></h2>
+									  <p>Sed at convallis erat</p>
+									  <a class="image-link" href="<?php echo $key_1_value = get_post_meta( get_the_ID(), 'gallerypic02', true );?>">View more</a>
+								  </figcaption>
+                </figure>
+               </div>
 
                         <div class="item">
                             <figure class="effect-bubba">
@@ -51,27 +50,29 @@ Template Name: Gallery
                             </figure>
                         </div>
 
-						<div class="item">
-                            <figure class="effect-bubba">
-								<img class="img-responsive" src="<?php echo $key_1_value = get_post_meta( get_the_ID(), 'gallerypic04', true );?>" alt="">
-								<figcaption>
-									<h2>Sed <span>nulla</span></h2>
-									<p>Sed at convallis erat</p>
-									<a class="image-link" href="<?php echo $key_1_value = get_post_meta( get_the_ID(), 'gallerypic04', true );?>">View more</a>
-								</figcaption>
-                            </figure>
-                        </div>
+						  <div class="item">
+                <figure class="effect-bubba">
+								  <img class="img-responsive" src="<?php echo $key_1_value = get_post_meta( get_the_ID(), 'gallerypic04', true );?>" alt="">
+  								<figcaption>
+  									<h2>Sed <span>nulla</span></h2>
+  									<p>Sed at convallis erat</p>
+  									<a class="image-link" href="<?php echo $key_1_value = get_post_meta( get_the_ID(), 'gallerypic04', true );?>">View more</a>
+  								</figcaption>
+                </figure>
+              </div>
 
-                    </div>
+            </div> <!-- gallery -->
 
-                </div>
-            </div>
-       </section>
-       </div><!-- #post-<?php the_ID(); ?> -->
+          </div> <!-- row -->
 
-      <?php if ( get_post_custom_values('comments') ) comments_template() // Add a custom field with Name and Value of "comments" to enable comments on this page ?>
+        </div> <!-- container -->
+      </section>
+      <div id="gallerybottom"><?php the_block( 'section2') ?></div>
+    </div><!-- #post-<?php the_ID(); ?> -->
 
-    </div><!-- #content -->
-		<?php get_sidebar(); ?>
+    <?php if ( get_post_custom_values('comments') ) comments_template() // Add a custom field with Name and Value of "comments" to enable comments on this page ?>
+
+  </div><!-- #content -->
+<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
