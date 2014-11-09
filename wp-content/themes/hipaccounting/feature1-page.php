af291cc1cfafc8cad4965937f4ab965280ccb6d4
q <qpham415@gmail.com>
2014-11-08 18:25:03 -0800

    restyle index page header and removed container

--- wp-content/themes/hipaccounting/feature1-page.php
+++ wp-content/themes/hipaccounting/feature1-page.php
@@ -4,14 +4,12 @@ Template Name: Feature1
 */
   get_header();
 ?>
-
-  <div id="container">
     <div id="content">
       <?php the_post(); ?>
 
       <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <div class="pHeader"> <?php echo $key_1_value = get_post_meta( get_the_ID(), 'pageHeader', true );?></div>
-        <div class="pSlogan"> <?php echo $key_1_value = get_post_meta( get_the_ID(), 'pageSlogan', true );?></div>
+        <div class="pHeader"> <h2><?php echo $key_1_value = get_post_meta( get_the_ID(), 'pageHeader', true );?></h2></div>
+        <div class="pSlogan"> <h3><?php echo $key_1_value = get_post_meta( get_the_ID(), 'pageSlogan', true );?></h3></div>
         <div class="entry-content">
           <div class="indexwrapper">
             <div id="section1wrap" class="headwrap">
@@ -25,9 +23,11 @@ Template Name: Feature1
             </div>
           </div>
           <div class="clear"></div>
-          <?php the_content(); ?>
-          <?php wp_link_pages('before=<div class="page-link">' . __( 'Pages:', 'your-theme' ) . '&after=</div>') ?>
-          <?php edit_post_link( __( 'Edit', 'your-theme' ), '<span class="edit-link">', '</span>' ) ?>
+          <div class="indexcontent">
+            <?php the_content(); ?>
+            <?php wp_link_pages('before=<div class="page-link">' . __( 'Pages:', 'your-theme' ) . '&after=</div>') ?>
+            <?php edit_post_link( __( 'Edit', 'your-theme' ), '<span class="edit-link">', '</span>' ) ?>
+          </div>
         </div><!-- .entry-content -->
       </div><!-- #post-<?php the_ID(); ?> -->
 
@@ -35,6 +35,5 @@ Template Name: Feature1
 
     </div><!-- #content -->
 		<?php get_sidebar(); ?>
-  </div><!-- #container -->
 
 <?php get_footer(); ?>
