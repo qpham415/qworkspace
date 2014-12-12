0e92710b0db4f86de236cef0aab01be15ce907a7
q <qpham415@gmail.com>
2014-12-11 21:58:04 -0800

    style single post page

--- wp-content/themes/hipaccounting/single.php
+++ wp-content/themes/hipaccounting/single.php
@@ -1,19 +1,19 @@
 <?php get_header(); ?>
- 
-        <div id="container">
-            <div id="content">
+
+        <div id="singleContainer">
+            <div id="singleContent">
 
 				<?php the_post(); ?>
 
                 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 					<h1 class="entry-title"><?php the_title(); ?></h1>
-					
+
 					<div class="entry-content">
 						<?php the_content(); ?>
 						<?php wp_link_pages('before=<div class="page-link">' . __( 'Pages:', 'hbd-theme' ) . '&after=</div>') ?>
 					</div><!-- .entry-utility -->
-					
+
 					<div class="entry-utility">
 					                    <?php printf( __( 'This entry was posted in %1$s%2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>. Follow any comments here with the <a href="%5$s" title="Comments RSS to %4$s" rel="alternate" type="application/rss+xml">RSS feed for this post</a>.', 'hbd-theme' ),
 					                        get_the_category_list(', '),
@@ -32,16 +32,16 @@
 					                        <?php _e( 'Both comments and trackbacks are currently closed.', 'hbd-theme' ) ?>
 					<?php endif; ?>
 					<?php edit_post_link( __( 'Edit', 'hbd-theme' ), "\n\t\t\t\t\t<span class=\"edit-link\">", "</span>" ) ?>
-                </div><!-- #post-<?php the_ID(); ?> -->           
- 
+                </div><!-- #post-<?php the_ID(); ?> -->
+
                 <div id="nav-below" class="navigation">
 						<?php previous_post_link( '%link', '<span class="meta-nav">&laquo;</span> %title' ) ?> <span style="color: #bbb;">&#8226;</span> <?php next_post_link( '%link', '%title <span class="meta-nav">&raquo;</span>' ) ?>
-				</div><!-- #nav-below -->     
-            
+				</div><!-- #nav-below -->
+
  				<?php comments_template('', true); ?>
 
             </div><!-- #content -->
         </div><!-- #container -->
- 
+
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
