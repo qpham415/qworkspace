f833f890fbbb0ad4bafbce13ef1a8300fa1c42c0
q <qpham415@gmail.com>
2014-12-11 19:31:27 -0800

    restyle index page

--- wp-content/themes/hipaccounting/index.php
+++ wp-content/themes/hipaccounting/index.php
@@ -28,11 +28,8 @@
 
 		<?php /* Microformatted category and tag links along with a comments link */ ?>
 		                    <div class="entry-utility">
-		                        <span class="cat-links"><span class="entry-utility-prep entry-utility-prep-cat-links"><?php _e( 'Posted in ', 'hbd-theme' ); ?></span><?php echo get_the_category_list(', '); ?></span>
-		                        <span class="meta-sep"> | </span>
-		                        <?php the_tags( '<span class="tag-links"><span class="entry-utility-prep entry-utility-prep-tag-links">' . __('Tagged ', 'hbd-theme' ) . '</span>', ", ", "</span>\n\t\t\t\t\t\t<span class=\"meta-sep\">|</span>\n" ) ?>
-		                        <span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'hbd-theme' ), __( '1 Comment', 'hbd-theme' ), __( '% Comments', 'hbd-theme' ) ) ?></span>
-		                        <?php edit_post_link( __( 'Edit', 'hbd-theme' ), "<span class=\"meta-sep\">|</span>\n\t\t\t\t\t\t<span class=\"edit-link\">", "</span>\n\t\t\t\t\t\n" ) ?>
+                            <span class="postDate"><?php $my_date = the_date('', 'Posted on ', '', FALSE); echo $my_date; ?></span>
+                            <?php edit_post_link( __( 'Edit', 'hbd-theme' ), "<span class=\"meta-sep\">|</span>\n\t\t\t\t\t\t<span class=\"edit-link\">", "</span>\n\t\t\t\t\t\n" ) ?>
 		                    </div><!-- #entry-utility -->
 		                </div><!-- #post-<?php the_ID(); ?> -->
 
